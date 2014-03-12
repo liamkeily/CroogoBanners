bbd6472d65dafd437d224a0b2aee30d4d562a9bf
Liam <liam@simplywebsites.com>
2014-03-12 17:12:13 +0000

    Add event handlers

---
+++ Config/events.php
@@ -0,0 +1,6 @@
+<?php
+$config = array(
+'EventHandlers' => array(
+'Banners.BannersEventHadler',
+),
+);
---
+++ Event/BannersEventHandler.php
@@ -0,0 +1,24 @@
+<?php
+App::uses('CakeEventListener','Event');
+
+class BannersEventHandler implements CakeEventListener {
+	public function implementedEvents() {
+		return array(
+		'Croogo.setupAdminData' => array(
+		'callable' => 'onSetupAdminData',
+		),
+		);
+	}
+
+	public function onSetupAdminData($event){
+		CroogoNav::add('banners', array(
+		'title' => __d('croogo', 'Banners'),
+		'url' => array(
+		'admin' => true,
+		'plugin' => 'banners',
+		'controller' => 'banners',
+		'action' => 'index',
+		),
+		));
+	}
+}
